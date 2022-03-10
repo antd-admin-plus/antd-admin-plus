843926035206e269d57bc2748f3fcafffb8981b4
cs <cs@nuapi.com>
2022-03-10 17:13:09 +0800

    first commit

---
+++ craco.config.js
@@ -0,0 +1,29 @@
+const CracoLessPlugin = require("craco-less");
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { "@primary-color": "#1DA57A" },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+  babel: {
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }], //装饰器
+      [
+        "import",
+        {
+          libraryName: "antd",
+          libraryDirectory: "es",
+          style: true, //设置为true即是less 按需加载后只需引入组件，无需再额外引入样式文件，babel会自动按需帮你完成样式的引入。这样打包出来的文件会更小。
+        },
+      ],
+    ],
+  },
+};
