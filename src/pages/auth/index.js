1da941c91b76beb3653eae2187c4912670a7f719
zerocsss <1756297001@qq.com>
2022-03-11 18:50:16 +0800

    add modules

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -4,7 +4,7 @@ import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 export default function RequireAuth() {
   // let auth = useAuth(); 待完善存储用户信息
-  let auth = {user: 'admin'};
+  let auth = {user: null};
   let location = useLocation();
   if (!auth.user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
