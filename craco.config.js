aa4553f769c4099c7f57731d6b5ad7141f249ba2
zerocsss <1756297001@qq.com>
2022-03-14 17:02:07 +0800

    add alias update layout

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,12 @@
 const CracoLessPlugin = require("craco-less");
+const path = require('path');
+
 module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve("src"),
+    }
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,1 +1,57 @@
-// layout
+import { useState } from 'react';
+import { Layout, Menu } from 'antd';
+import './index.less';
+
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  DashboardOutlined,
+  LineChartOutlined
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
+
+export default function BaseLayout({children}) {
+  const [collapsed, toggleCollapsed] = useState(false);
+
+  return (
+    <Layout className='layout'>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo text-white font-semibold text-xl p-4">Admin Plus</div>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<DashboardOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="2" icon={<UserOutlined />}>
+              Users
+            </Menu.Item>
+            <Menu.Item key="3" icon={<LineChartOutlined />}>
+              Charts
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0, color: '#fff' }}>
+            {
+              collapsed ? <MenuUnfoldOutlined onClick={() => toggleCollapsed(false)}></MenuUnfoldOutlined> : <MenuFoldOutlined onClick={() => toggleCollapsed(true)}></MenuFoldOutlined>
+            }
+            {/* {React.createElement(, {
+              className: 'trigger',
+              onClick: toggle,
+            })} */}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            {children}
+          </Content>
+        </Layout>
+      </Layout>
+  )
+}
